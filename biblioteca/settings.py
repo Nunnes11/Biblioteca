2a0b447b8a7a8fe20994212d2c1eecad929adf8d
Nunnes11 <frannes09@gmail.com>
2023-11-28 20:41:07 -0300

    add banco de dados

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -74,12 +74,25 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = { 
+
+    "default": { 
+
+        "ENGINE": "django.db.backends.postgresql", 
+
+        "NAME": "biblioteca", 
+
+        "USER": "postgres", 
+
+        "PASSWORD": "senac", 
+
+        "HOST": "127.0.0.1", 
+
+        "PORT": "5432", 
+
+    } 
+
+} 
 
 
 # Password validation
